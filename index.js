bac4fe16b6dd92c3715d5c648229a4dc6946942d
Gabriel Mičko <micsko@gmail.com>
2017-05-06 01:37:07 +0200

    Generalize things, retry button

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var randomstring = require('randomstring');
 var qr = require('qr-image');
+var config = require('config');
+
+var url = config.get('url');
+console.log(url);
 
 function getRS() {
-	return randomstring.generate({
-	  length: 12,
-	  charset: 'alphabetic'
-	});
+  return randomstring.generate({
+    length: 12,
+    charset: 'alphabetic'
+  });
 }
 
-app.use(bodyParser.json({
-	'limit': '50mb'
-}));
+app.use(
+  bodyParser.json({
+    limit: '50mb'
+  })
+);
 
-app.use(bodyParser.urlencoded({
-  'extended': true,
-	'limit': '50mb'
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: '50mb'
+  })
+);
 
 app.use(express.static('public'));
 
 app.get('/result', function(req, resp) {
-	resp.sendFile(__dirname + '/public/result.html');
+  resp.sendFile(__dirname + '/public/result.html');
 });
 
 app.get('*', function(req, resp) {
-	resp.sendFile(__dirname + '/public/index.html');
+  resp.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/savecanvas', function(req, resp) {
-	var base64Data = req.body.canvas.replace(/^data:image\/png;base64,/, "");
-	var randomString = getRS();
+  var base64Data = req.body.canvas.replace(/^data:image\/png;base64,/, '');
+  var randomString = getRS();
 
-	fs.writeFile('./public/img/generated/pic/' + randomString + '.png', base64Data, 'base64', function(err) {
-		var qr_svg = qr.image('https://mozilla.webrtc.rocks/img/generated/pic/' + randomString + '.png', { type: 'svg' });
-		qr_svg.pipe(fs.createWriteStream('./public/img/generated/qr/' + randomString + '.svg'));
-		resp.send({
-			'hash': randomString,
-		});
-	});
+  fs.writeFile(
+    './public/img/generated/pic/' + randomString + '.png',
+    base64Data,
+    'base64',
+    function(err) {
+      var qr_svg = qr.image(
+        url + '/img/generated/pic/' + randomString + '.png',
+        { type: 'svg' }
+      );
+      qr_svg.pipe(
+        fs.createWriteStream(
+          './public/img/generated/qr/' + randomString + '.svg'
+        )
+      );
+      resp.send({
+        hash: randomString
+      });
+    }
+  );
 });
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Running Mozilla VR Social plugin on port 3000!');
 });
--- public/js/moz.js
+++ public/js/moz.js
@@ -1,19 +1,22 @@
-var cam = $('.fa-camera');
+var camButton = $('.fa.fa-camera');
 var toggle = $('.toggle .fa');
 var takeSnap = $('.takesnap');
+var retryButton = $('.retry');
 var canvas = document.querySelector('canvas');
 var video = document.querySelector('video');
 var context = canvas.getContext('2d');
-var mozImage = $('.slide.mozimage .mover .img.on').attr('data-img');
-var eveImage = $('.slide.eveimg .mover .img.on').attr('data-img');
+var defaultImageSrc = '/logos/shape.png';
 var snapShot = false;
+var localStream = false;
 
 var constraints = {
   audio: false,
-  video: {mandatory: {
-    maxWidth: 640,
-    maxHeight: 480
-  }}
+  video: {
+    mandatory: {
+      maxWidth: 640,
+      maxHeight: 480
+    }
+  }
 };
 
 takeSnap.on('click', function() {
@@ -21,114 +24,130 @@ takeSnap.on('click', function() {
 
   $.ajax({
     url: '/savecanvas',
-    method: "POST",
-    dataType: "json",
-    data: { canvas : canvas.toDataURL("image/png") },
-    dataType: "html",
+    method: 'POST',
+    dataType: 'json',
+    data: { canvas: canvas.toDataURL('image/png') },
+    dataType: 'html',
     success: function(result) {
       result = JSON.parse(result);
       alert('Picture has been generated!');
-      $('.snap').attr('href', '/result#' + result.hash);
+      $('.snapshots-container').prepend(
+        '<a target="_blank" href="' +
+          '/result#' +
+          result.hash +
+          '"><img src="/img/generated/pic/' +
+          result.hash +
+          '.png"></a>'
+      );
     }
   });
 });
 
+retryButton.on('click', function() {
+  snapShot = false;
+  startVideo();
+});
+
+//Toggle the footer menu
 toggle.on('click', function() {
   var $this = $(this).parents('.slideshow');
-  if($this.hasClass('open')) {
+  if ($this.hasClass('open')) {
     $this.removeClass('open');
-  }
-  else {
+  } else {
     $this.addClass('open');
     $('html, body').animate({ scrollTop: $(document).height() }, 500);
   }
-})
+});
 
 function handleSuccess(stream) {
-  cam.hide();
+  camButton.hide();
   video.src = window.URL.createObjectURL(stream);
+  localStream = stream;
   canvas.width = 640;
-  canvas.height = 480
+  canvas.height = 480;
 }
-baseMozImg = new Image();
-baseMozImg.src = mozImage;
+var defaultImageElement = new Image();
+defaultImageElement.src = defaultImageSrc;
 
-// baseEveImg = new Image();
-// baseEveImg.src = eveImage;
-
-function make_base() {
-  context.drawImage(baseMozImg, 0, 0, 640, 480);
+function takeAPicture(picture) {
+  context.drawImage(picture, 0, 0);
+  context.drawImage(defaultImageElement, 0, 0, 640, 480);
   //context.drawImage(baseEveImg, 502, 352, 128, 128);
 }
 
-
-function playy() {
-  var $this = this; //cache
+function playVideoOnCanvas() {
+  var picture = this; //cache
   (function loop() {
-      if (!$this.paused && !$this.ended) {
-          if(snapShot === false) {
-            context.drawImage($this, 0, 0);
-            make_base();
-            setTimeout(loop, 1000 / 10); // drawing at 30fps
-          }
+    if (!picture.paused && !picture.ended) {
+      if (snapShot === false) {
+        takeAPicture(picture);
+        setTimeout(loop, 1000 / 10);
+      } else {
+        video.pause();
+        video.src = '';
+        localStream.getTracks()[0].stop();
       }
+    }
   })();
 }
 
-video.addEventListener('play', playy);
-function handleError() {
+video.addEventListener('play', playVideoOnCanvas);
+function handleError() {}
 
+function startVideo() {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(handleSuccess)
+      .catch(handleError);
+  } else {
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+    navigator.getUserMedia({ video: true }, handleSuccess, handleError);
+  }
 }
 
-cam.on('click', function() {
-  if(navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
-	}
-	else {
-		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-		navigator.getUserMedia({video:true}, handleSuccess, handleError);
-	}
-});
+camButton.on('click', startVideo);
 
-var mozimage = $('.slide.mozimage');
-var firstMozImg = $('.slide.mozimage .holder .img').eq(0);
-
-$('.slide .left, .slide .right').on('click', function() {
-  var mozImage = !!$(this).parents('.slide.mozimage').length;
-  var $this = $(this);
-  var dir = 'left';
-  if($this.hasClass('right')) {
-    dir = 'right';
-  }
-  var images = $this.parents('.slide').find('.holder .img');
-  var mover = $this.parents('.slide').find('.mover');
-  var index = images.filter('.on').index();
-  var total = images.length;
-
-  if(dir === 'left') {
-    newIndex = index - 1;
-  }
-  else {
-    newIndex = index + 1;
-  }
-
-  if(newIndex >= 0 && newIndex < total) {
-    images.eq(index).removeClass('on');
-    var size = 75;
-    if(newIndex > 0) {
-      size = size - (newIndex * 150);
-    }
-    mover.css('margin-left', size + 'px');
-    var newImage = images.eq(newIndex);
-    newImage.addClass('on');
-
-    if(mozImage) {
-      baseMozImg = new Image();
-      baseMozImg.src = newImage.attr('data-img');
-    }
-    else {
-      baseEveImg = new Image();
-      baseEveImg.src = newImage.attr('data-img');
-    }
-  }
-});
+//var mozimage = $('.slide.mozimage');
+//var firstMozImg = $('.slide.mozimage .holder .img').eq(0);
+//
+// $('.slide .left, .slide .right').on('click', function() {
+//   var mozImage = !!$(this).parents('.slide.mozimage').length;
+//   var $this = $(this);
+//   var dir = 'left';
+//   if ($this.hasClass('right')) {
+//     dir = 'right';
+//   }
+//   var images = $this.parents('.slide').find('.holder .img');
+//   var mover = $this.parents('.slide').find('.mover');
+//   var index = images.filter('.on').index();
+//   var total = images.length;
+//
+//   if (dir === 'left') {
+//     newIndex = index - 1;
+//   } else {
+//     newIndex = index + 1;
+//   }
+//
+//   if (newIndex >= 0 && newIndex < total) {
+//     images.eq(index).removeClass('on');
+//     var size = 75;
+//     if (newIndex > 0) {
+//       size = size - newIndex * 150;
+//     }
+//     mover.css('margin-left', size + 'px');
+//     var newImage = images.eq(newIndex);
+//     newImage.addClass('on');
+//
+//     if (mozImage) {
+//       defaultImageElement = new Image();
+//       defaultImageElement.src = newImage.attr('data-img');
+//     } else {
+//       baseEveImg = new Image();
+//       baseEveImg.src = newImage.attr('data-img');
+//     }
+//   }
+// });
